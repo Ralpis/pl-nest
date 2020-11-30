2342f7423f9b54aff9d1d2af984f9872559bf48a
Ralpis <rotjsdn1@naver.com>
2020-11-30 21:41:56 +0900

    수정함:        src/movies/movies.service.spec.ts

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -17,53 +17,95 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
-describe("getAll", ()=>{
-
-  it("shoud return an array",()=>{
-    const result = service.getAll();
-    expect(result).toBeInstanceOf(Array);
-  });
-});
-
-describe('getOne', () => {
-  it("shoud return a movie",()=>{
-    service.create({
-      title:'Test Movie',
-      genres:['test'],
-      year: 2000,
+  describe('getAll', () => {
+    it('shoud return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
     });
-
-    const movie = service.getOne(1);
-    expect(movie).toBeDefined();
-    expect(movie.id).toEqual(1);
   });
 
-  it("shoud throw 404 error", ()=>{
-    try{
-      service.getOne(999);
-    }catch(e){
-      expect(e).toBeInstanceOf(NotFoundException);
-      expect(e.message).toEqual('Movie with Id 999 not found.');
-    }
-  })
+  describe('getOne', () => {
+    it('shoud return a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
 
-});
-describe('deleteOne', ()=>{
-
-  it("deletes a movie",()=>{
-    service.create({
-      title:'Test Movie',
-      genres:['test'],
-      year: 2000,
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
     });
-    const beforeDelete = service.getAll().length;
-    service.deleteOne(1);
-    const afterDelete = service.getAll().length;
-    expect(afterDelete).toBeLessThan(beforeDelete);
 
+    it('shoud throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with Id 999 not found.');
+      }
+    });
+  });
+  describe('deleteOne', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create',()=>{
+
+    it("should create a movie",()=>{
+
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const afterCreate = service.getAll().length;
+      console.log(beforeCreate,afterCreate);
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+    
   })
 
-});
+  describe('update',()=>{
+    it("should update a movie",()=>{
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
 
+      service.update(1,{
+        title:'Updated Test',
+      });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated Test');
 
-});
+    });
+    it('shoud throw 404 error', () => {
+      try {
+        service.update(999,{});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      
+      }
+    });
+  });
+  })
